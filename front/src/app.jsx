7fc3e6a48cc8bbfce5684a4aaa3adc7c13d93dd4
Camarchepas <36743875+Camarchepas@users.noreply.github.com>
2019-11-08 14:19:43 +0100

    creation fichiers de base

---
+++ front/src/app.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+/*
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+
+*/
+class App extends React.component {
+  constructor()
+  render() {
+    return(<div>hello</div>);
+  }
+
+}
+
+export default App;

78cb22a1d45bfb249dfa1c94ce93349635cdb3d0
Olivier Lemoine <olivier@le-moine.fr>
2019-11-08 16:19:09 +0100

    comp

---
+++ front/src/AddWord/add-word.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+
+export default class extends React.Component {
+    constructor() {
+        super()
+
+        this.state = {
+            article: "Der",
+            wordName: "",
+        };
+
+        this.handleUpdateName = (ev) => {
+            this.setState({ wordName: ev.target.value });
+        };
+
+        this.generateHandleUpdateArticle = (article) => () => {
+            this.setState({ article: article });
+        };
+
+        this.handleSubmit = () => {
+            console.log(this.state);
+            fetch({ method: 'POST', body: JSON.stringify({}) }).then().catch(reason => console.log(reason));
+        };
+    }
+
+
+    render() {
+        return <div className="row">
+            <div className="card-panel col m6 offset-m3 s12">
+                <form style={{ margin: "10px" }} action="#" className="row">
+                    <ul className="col s6">
+                        {
+                            ["Der", "Die", "Das"].map((article, i) => (
+                                <li key={article}>
+                                    <label>
+                                        <input name="article" type="radio" onChange={this.generateHandleUpdateArticle(article)}
+                                            checked={article === this.state.article} />
+                                        <span>{article}</span>
+                                    </label>
+                                </li>
+                            ))
+                        }
+                    </ul>
+                    <div className="col s6">
+                        <input type="text" value={this.state.wordName} onChange={this.handleUpdateName} />
+                        <button className="btn" type="submit" onClick={this.handleSubmit}>Submit</button>
+                    </div>
+                </form>
+            </div>
+        </div>;
+    }
+}

861adc1b70c65c9b60dc81cfa9a2c0830d84fed7
Olivier Lemoine <olivier@le-moine.fr>
2019-11-08 15:11:04 +0100

    update api

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,21 +1,8 @@
 import express from 'express';
 import api from "./api";
-import fs from 'fs';
 
 const app = express();
 
-app.use(express.static("../front/build"))
-    .use(express.static("../front/statics"))
-    .use('/api', api)
-    .get('/', (req, res) => {
-        fs.readFile("../front/statics/index.html", (err, data) => {
-            if (err) {
-                console.error(err);
-                res.status(500).end();
-            } else {
-                res.end(data);
-            }
-        });
-    });
+app.use('/api', api)
 
-app.listen(8000, () => console.log('Started on localhost:8000'));
+app.listen(8010, () => console.log('API ready on localhost:8010'));

e2dded7e02380ddc6b3c40fe439222eb2e8a8550
Olivier Lemoine <olivier@le-moine.fr>
2019-11-08 14:41:44 +0100

    update front

--- run.js
+++ run.js
@@ -1,15 +1,21 @@
 let cp = require('child_process');
 
-cp.exec('npm run watch', { cwd: "./front/", }, (err, stdout, stderr) => {
+let react = cp.exec('npm run start', { cwd: "./front/", }, (err, stdout, stderr) => {
     if (err) {
         throw err;
     }
 });
-cp.exec('npm run watch', { cwd: "./server/", }, (err, stdout, stderr) => {
+react.stdout.on('data', data => console.log(data));
+react.stderr.on('data', data => console.error(data));
+
+let ts = cp.exec('npm run watch', { cwd: "./server/", }, (err, stdout, stderr) => {
     if (err) {
         throw err;
     }
 });
+ts.stdout.on('data', data => console.log(data));
+ts.stderr.on('data', data => console.error(data));
+
 let serv = cp.exec('npm start', { cwd: "./server/", }, (err, stdout, stderr) => {
     if (err) {
         throw err;

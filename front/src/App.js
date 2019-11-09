b88d29f8fc37fdbf94efee0579ef0ef55dc1bffd
Olivier Lemoine <olivier@le-moine.fr>
2019-11-09 16:07:01 +0100

    fetcher comp

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import AddWord from './AddWord/AddWord';
+import Waiting from './Waiting/Waiting';
 
 function App() {
     return (
         <div>
+            <Waiting onLoaded={} />
             <AddWord />
         </div>
     );
---
+++ front/src/Waiting/Waiting.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default class extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    async componentDidMount() {
+        let res = await fetch('/api/words').catch(reason => console.log(reason));
+        let val = await res.json();
+        if (res.status !== 200) {
+            this.props.onLoaded(val, null);
+        } else if (!val.data) {
+            this.props.onLoaded(val, null);
+        } else {
+            this.props.onLoaded(null, val.data);
+        }
+    }
+
+    render() {
+
+    }
+}

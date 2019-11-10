4e1f5c4a7419b79971fba585816560f4502d79a4
Olivier Lemoine <olivier@le-moine.fr>
2019-11-10 11:10:08 +0100

    add like to router

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,37 +1,41 @@
 import React from 'react';
 import './App.css';
-// import AddWord from './AddWord/AddWord';
+import AddWord from './AddWord/AddWord';
 import DropWord from './Games/WordDrop/WordDrop';
-import Burger from "./Menu/Burger";
+// import Burger from "./Menu/Burger";
+import Menu from "./Menu/Menu";
 import {
     BrowserRouter as Router,
     Switch,
     Route,
 } from "react-router-dom";
 
-function App() {
-    let list = [
-        {
-            name: "test",
-            article: "der",
-        }
-    ];
+export default class extends React.Component {
+    render() {
+        let list = [
+            {
+                name: "test",
+                article: "der",
+            }
+        ];
 
-    return (
-        <Router>
-            <div className="side-bar">
-                <Burger />
-            </div>
-            <div className="content">
-                <Switch>
-                    <Route path="/moon-defender">
-                        <DropWord words={list} />
-                    </Route>
-                    <Route path="/"></Route>
-                </Switch>
-            </div>
-        </Router>
-    );
+        return (
+            <Router>
+                <div id="container">
+                    <Menu />
+                    <div className="content">
+                        <Switch>
+                            <Route path="/moon-defender">
+                                <DropWord words={list} />
+                            </Route>
+                            <Route path="/add-word">
+                                <AddWord />
+                            </Route>
+                            <Route path="/"></Route>
+                        </Switch>
+                    </div>
+                </div>
+            </Router>
+        );
+    }
 }
-
-export default App;
--- front/src/Menu/Menu.jsx
+++ front/src/Menu/Menu.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
-import './Burger.css'
+import './Menu.css'
 import { Link } from 'react-router-dom';
 
 export default class extends React.Component {
     render() {
         return (
             <div>
-                <br />
-                <Link to="/">About</Link><br /><br />
-                <Link to="/">Game 1</Link><br />
-                <Link to="/moon-defender">Moon Defender</Link><br /><br />
-                <Link to="/">Add word</Link>
+                <div className="menu show-on-large">
+                    <ul>
+                        <li>
+                            <Link to="/">About</Link>
+                        </li>
+                        <br />
+                        <li>
+                            <Link to="/">Game 1</Link>
+                        </li>
+                        <li>
+                            <Link to="/moon-defender">Moon Defender</Link>
+                        </li>
+                        <br />
+                        <li>
+                            <Link to="/add-word">Add word</Link>
+                        </li>
+                    </ul>
+                </div>
             </div>
         );
     }

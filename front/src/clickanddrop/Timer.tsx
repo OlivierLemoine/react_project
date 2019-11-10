88ca15d324f77911a30172089491be4a6c2bfe2f
Olivier Lemoine <olivier@le-moine.fr>
2019-11-10 15:33:12 +0100

    add interval

--- front/src/clickanddrop/Timer.tsx
+++ front/src/clickanddrop/Timer.tsx
@@ -1,15 +1,29 @@
 import React from 'react';
 
+type IntervalFn = number;
+
 type Props = {
     sec: number,
     onEnd: () => void,
 }
 
 type State = {
-
+    millis: number,
 }
 
 export default class extends React.Component<Props, State>{
+    intervalUpdate: null | IntervalFn = null;
+
+    componentWillUnmount() {
+        clearInterval(this.intervalUpdate);
+    }
+
+    componentDidMount() {
+        this.intervalUpdate = setInterval(() => {
+            this.setState({ millis: this.state.millis + 10 });
+        }, 10);
+    }
+
     render() {
         return (
             <div>
